5efb4e7bde29c400d1b797459d679c5d9413e904
Ruben <PatelR53@cardiff.ac.uk>
2025-04-05 20:31:32 +0100

    quickSort v1

--- sorting1.java
+++ sorting1.java
@@ -5,28 +5,39 @@ import java.util.concurrent.ThreadLocalRandom;
 
 class Main {   
 
-
-    public static int partition(int array[],int low, int high) {
-        int pivot = array[high];
+    public static int partition(ArrayList<Integer> array ,int low, int high) {
+        int pivot = array.get(high);
         int index = low - 1; // this points to the index of the next value to be added if it is <= pivot 
 
         for (int j = low; j < high; j++) {    // iterates through each value in array
-            if (array[j] < pivot) {
+            if (array.get(j) < pivot) {
                 index++; // increases value of i
 
-                int temp = array[index];   // swapping element at index with element at j
-                array[index] = array[j];
-                array[j] = temp;
+                int temp = array.get(index);   // swapping element at index with element at j
+                array.set(index, array.get(j));
+                array.set(j,temp);
 
             }
         }     // for loop and if statement closed
-
-
+        // Swap the pivot element with the element at index i + 1
+        int temp = array.get(index + 1);
+        array.set(index + 1, array.get(high));
+        array.set(high, temp);
+        return (index + 1);
     }
 
-    public static void quickSort(int[] array, int low, int high){
+
+    public static void quickSort(ArrayList<Integer> array, int low, int high){
         if (high <= low) return; //base case
+    
+        // Partition the array and get the pivot index
+        int pivot = partition(array, low, high);
+        // Recursively sort the sub-arrays
+        quickSort(array, low, pivot - 1);  // Left sub-array
+        quickSort(array, pivot + 1, high); // Right sub-array
     }
+
+
     public static void main(String[] args) {
     //     ArrayList<Integer> day1 = new ArrayList<>();
     //     ArrayList<Integer> day2 = new ArrayList<>();
@@ -56,8 +67,8 @@ class Main {
         for (int i=0; i != 500000; i++) {
             day7.add(ThreadLocalRandom.current().nextInt(1000, 500000 + 1));
         }
+        quickSort(day7, 0, (day7.size() - 1));
         System.out.println(day7); 
-
 
 
 
